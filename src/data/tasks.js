3ef3f50bef65b6f5e050bc32ba0ebb9c268891a0
AlexDev <galexramirez@yahoo.com>
2024-03-09 22:18:51 -0500

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "mi primera tarea",
+    description: "descripcion de mi primera tarea",
+  },
+  {
+    id: 1,
+    title: "mi segunda tarea",
+    description: "descripcion de mi segunda tarea",
+  },
+  {
+    id: 2,
+    title: "mi tercera tarea",
+    description: "descripcion de mi tercera tarea",
+  },
+];
